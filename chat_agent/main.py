c7276834569e884350bbe164345ab2d6c30d64e6
a-regmi <aakash.regmi@outlook.com>
2025-06-14 21:51:29 -0400

    Chat Agent Final

--- chat_agent/main.py
+++ chat_agent/main.py
@@ -3,15 +3,43 @@ from langchain_openai import ChatOpenAI
 from langchain.tools import tool
 from langgraph.prebuilt import create_react_agent
 from dotenv import load_dotenv
+import os
 
 load_dotenv()
 
 def main():
-    model = ChatOpenAI(temperature = 0)
-
-    tools = []
-    agent_executor = create_react_agent(model, tools)
-
-    print("Hello Welcome to the aRegmi's Chat _ AI Assistant \n Type 'quit' to exit.")
-    print("I am a specialized AI-math wizard, tell me what kind of calc you need? \n or, simply Lets chat: ")
+    api_key = os.getenv('OPENAI_API_KEY')
+    print("API Key loaded:", bool(api_key))
+    if not api_key:
+        raise ValueError("OPENAI_API_KEY not found in env variables")
     
+    try: 
+        model = ChatOpenAI(temperature = 0)
+
+        tools = []
+        agent_executor = create_react_agent(model, tools)
+
+        print("Hello Welcome to the aRegmi's Chat Agent \nType 'quit' to exit.")
+        print("I am a chat agent, tell me what are you curious about today ? \nor, simply Lets chat: ")
+        
+        while True:
+            user_input = input("\nYou: ").strip()
+
+            if user_input == "quit":
+                break
+
+            print("\nAssistant: ", end="") #No new default line 
+            for chunk in agent_executor.stream(
+                {"messages" : [HumanMessage(content = user_input)]}
+            ):
+                if "agent" in chunk and "messages" in chunk["agent"]:
+                    for message in chunk["agent"]["messages"]:
+                        print(message.content, end = "")
+            print()
+
+    except Exception as e:
+        print(f"Error initializing ChatOpenAI: {str(e)}")
+        return
+
+if __name__ == "__main__":
+    main()
